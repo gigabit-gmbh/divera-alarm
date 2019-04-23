65d232137a72d12e5ae9964ec46d2ecf972503e3
Thomas Helmrich <thomas@gigabit.de>
2019-04-23 14:30:02 +0200

    updated path for xsession to pi user

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ mkdir processing processing/mails processing/mailexport processing/ocr
 apt install poppler-utils tesseract-ocr ocrmypdf tesseract-ocr-deu libnotify-bin notification-daemon cups notify-osd at-spi2-core libnotify-bin dbus
 usermod -a -G lpadmin pi
 
-cat <<EOT >> ~/.xsessionrc
+cat <<EOT >> /home/pi/.xsessionrc
 #!/bin/sh
 /usr/lib/notification-daemon/notification-daemon &
 EOT
